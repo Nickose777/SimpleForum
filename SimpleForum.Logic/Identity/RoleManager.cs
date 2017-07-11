823c329461b8be47e5ea7bda1a8a34cb7f8103b0
Nickose777 <my_personal@mail.ua>
2017-07-11 15:43:21 +0300

    Added untracked files

---
+++ SimpleForum.Logic/Identity/RoleManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SimpleForum.Core;
+using SimpleForum.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleForum.Logic.Identity
+{
+    class RoleManager : RoleManager<ApplicationRole, string>
+    {
+        public RoleManager(ForumDbContext context)
+            : base(new RoleStore<ApplicationRole>(context)) { }
+    }
+}
