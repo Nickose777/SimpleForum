7cee4c4046bddebb54891c358077f9d72e5d2534
Nickose777 <my_personal@mail.ua>
2017-07-12 19:18:47 +0300

    Included two untracked files

---
+++ SimpleForum.Logic/DTO/Message/MessageCreateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleForum.Logic.DTO.Message
+{
+    public class MessageCreateDTO
+    {
+        public string Text { get; set; }
+
+        public string SenderLogin { get; set; }
+
+        public int TopicId { get; set; }
+    }
+}
---
+++ SimpleForum/Controllers/MessageController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using SimpleForum.Helpers;
+using SimpleForum.Logic.Contracts;
+using SimpleForum.Logic.DTO.Message;
+using SimpleForum.Logic.Infrastructure;
+using SimpleForum.Models.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleForum.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly IMessageService service;
+
+        public MessageController(IMessageService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(MessageCreateModel model)
+        {
+            bool success = true;
+            string html = "";
+
+            if (!ModelState.IsValid)
+            {
+                success = false;
+            }
+            else
+            {
+                MessageCreateDTO messageDTO = Mapper.Map<MessageCreateModel, MessageCreateDTO>(model);
+                ServiceMessage serviceMessage = service.Create(messageDTO);
+
+                if (!serviceMessage.Succeeded)
+                {
+                    foreach (string error in serviceMessage.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+
+                    success = false;
+                }
+            }
+
+            if (!success)
+            {
+                html = RenderHelper.PartialView(this, "~/Views/Message/Create.cshtml", model);
+            }
+
+            return Json(new { success = success, html = html });
+        }
+    }
+}
